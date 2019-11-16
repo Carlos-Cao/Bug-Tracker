fb3689ad48ab2fd826cbd55feccf33eb25038fb1
carlos-cao <carlos.cao2@gmail.com>
2019-11-16 21:54:34 +1300

    New Commit

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+import mysql.connector
+import tkinter as tk
+import tkinter.ttk
+from report import *
+
 class Menu(object):
-    def __init__(self):
-        pass
+    def __init__(self, connection):
+        self._bug = Bug(connection)
+        #Setup menu
+        self.root = tk.Tk()
+        self.root.title("Bug Tracker")
+        self.frame = tk.Frame(self.root)
+        self.frame.pack(padx = 50, pady = 50)
+        #Create grid headings
+        columns = ["ID", "Type", "Title", "Assigned to", "Priority", "Status", "Description"]
+        self.tree = tkinter.ttk.Treeview(self.frame, columns = columns, show = "headings")
+        self.tree.grid(in_=self.frame, row = 0, column = 0, columnspan = 5, sticky = tk.NSEW)
+        for column in columns:
+            self.tree.heading(column, text = column.title())
+        self.menu_buttons()
 
-    def start(self):
-        pass
+    def menu_buttons(self):
+        #Add bug report button
+        add_button = tk.Button(self.frame, text = "Add Bug Report", command = None, width = 20)
+        add_button.grid(in_= self.frame, row = 1, column = 0, sticky = tk.W)
+        #Exit button
+        exit_button = tk.Button(self.frame, text = "Exit", command = self.close, width = 20)
+        exit_button.grid(in_= self.frame, row = 1, column = 4, sticky = tk.E)
+        #Remove bug report button
+        remove_button = tk.Button(self.frame, text = "Remove Bug Report", command = None, width = 20)
+        remove_button.grid(in_= self.frame, row = 1, column = 2)
+        self.list_bugs()
+        self.root.mainloop()
 
-    def populate(self):
-        pass
+    def list_bugs(self):
+        #Populate and refresh data
+        self.tree.delete(*self.tree.get_children())
+        for bug in self._bug.list_bugs():
+            values = (bug.id, bug.types, bug.title, bug.assigned_to, bug.priority, bug.status, bug.description)
+            self.tree.insert("", "end", values = values)
 
     def add_bug(self):
         pass
@@ -20,6 +51,9 @@ class Menu(object):
     def update_bug(self):
         pass
 
+    def close(self):
+        self.root.destroy()
+
 class EditWindow(object):
     def __init__(self):
         pass
@@ -32,3 +66,14 @@ class EditWindow(object):
 
     def close(self):
         pass
+
+
+if __name__ == '__main__':
+    print('Connecting to database...')
+    connection = mysql.connector.connect(user='root',
+                                   database='bug_tracker',
+                                   charset='utf8')
+    print('Database connected')
+    application = Menu(connection)
+    application.start()
+    conn.close()
--- report.py
+++ report.py
@@ -1,7 +1,7 @@
 class Data(object):
     def __init__(self, types, title, id = None):
         self.id = id
-        self.type = types
+        self.types = types
         self.title = title
         self.assigned_to = None
         self.priority = None
@@ -9,14 +9,24 @@ class Data(object):
         self.description = None
 
 class Bug(object):
-    def __init__(self):
-        pass
+    def __init__(self, connection):
+        self._connection = connection
     
     def add(self):
         pass
 
     def list_bugs(self):
-        pass
+        cursor = self._connection.cursor()
+        query = "SELECT id, bug_type, title, assigned_to, priority, bug_status, description FROM bugtracker ORDER BY id"
+        cursor.execute(query)
+        for (id, types, title, assigned_to, priority, status, description) in cursor:
+            bug = Data(types, title, id)
+            bug.assigned_to = assigned_to
+            bug.priority = priority
+            bug.status = status
+            bug.description = description
+            yield bug
+        cursor.close()
     
     def update(self):
         pass
