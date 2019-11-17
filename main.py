e8d8fdd2d0c259b00bc5cb91e3d643a7ce0635f1
carlos-cao <carlos.cao2@gmail.com>
2019-11-17 13:07:00 +1300

    Commit

--- main.py
+++ main.py
@@ -17,11 +17,14 @@ class Menu(object):
         self.tree.grid(in_=self.frame, row = 0, column = 0, columnspan = 5, sticky = tk.NSEW)
         for column in columns:
             self.tree.heading(column, text = column.title())
+        self.tree.bind("<Double-1>", self.edit_bug)
         self.menu_buttons()
+        self.list_bugs()
+        self.root.mainloop()
 
     def menu_buttons(self):
-        #Add bug report button
-        add_button = tk.Button(self.frame, text = "Add Bug Report", command = None, width = 20)
+        #Add bug report buttons
+        add_button = tk.Button(self.frame, text = "Add Bug Report", command = self.add_bug, width = 20)
         add_button.grid(in_= self.frame, row = 1, column = 0, sticky = tk.W)
         #Exit button
         exit_button = tk.Button(self.frame, text = "Exit", command = self.close, width = 20)
@@ -29,8 +32,6 @@ class Menu(object):
         #Remove bug report button
         remove_button = tk.Button(self.frame, text = "Remove Bug Report", command = None, width = 20)
         remove_button.grid(in_= self.frame, row = 1, column = 2)
-        self.list_bugs()
-        self.root.mainloop()
 
     def list_bugs(self):
         #Populate and refresh data
@@ -40,40 +41,176 @@ class Menu(object):
             self.tree.insert("", "end", values = values)
 
     def add_bug(self):
-        pass
+        bug = Data("","","","","","","")
+        self.view_bug(bug, self._save_bug)
 
-    def edit_bug(self):
-        pass
+    def _save_bug(self, bug):
+        self._bug.add(bug)
+        self.list_bugs()
 
-    def view_bug(self):
-        pass
+    def edit_bug(self, event):
+        item = self.tree.item(self.tree.focus())
+        item_id = item["values"][0]
+        item_bug_type = item["values"][1]
+        item_title = item["values"][2]
+        item_assigned_to = item["values"][3]
+        item_priority = item["values"][4]
+        item_bug_status = item["values"][5]
+        item_description = item["values"][6]
+        bug = Data(item_id, item_bug_type, item_title, item_assigned_to, item_priority, item_bug_status, item_description)
+        self.view_bug(bug, self._update_bug)
 
-    def update_bug(self):
-        pass
+    def _update_bug(self, bug):
+        self._bug.update(bug)
+        self.list_bugs()
+
+    def view_bug(self, bug, action):
+        window = EditWindow(self, bug, action)
+        self.root.wait_window(window.root)
 
     def close(self):
         self.root.destroy()
 
 class EditWindow(object):
-    def __init__(self):
-        pass
+    def __init__(self, parent, bug, saveAction):
+        self._parent = parent.root
+        self.root = tk.Toplevel(parent.root)
+        self.root.title("Bug report")
+        self.root.transient(parent.root)
+        self.root.grab_set()
+        self.saveAction = saveAction
+        self._bug = bug
 
-    def add_widgets(self):
-        pass
+        self.frame = tk.Frame(self.root)
+        self.frame.pack(side = tk.TOP, fill = tk.BOTH, expand = tk.Y, padx= 10, pady = 10)
+
+        last_row = self.add_widgets(bug)
+        self._save_button = tk.Button(self.frame, text = "Save", command = self.save_bugs, width = 20, padx = 5, pady = 5)
+        self._save_button.grid(in_ = self.frame, row = last_row + 7, column = 1, sticky = tk.E)
+
+        exit_button = tk.Button(self.frame, text = "Close", command = self.close, width = 20, padx = 5, pady = 5)
+        exit_button.grid(in_ = self.frame, row = last_row + 8, column = 1, sticky = tk.E)
+    
+    def close(self):
+        self.root.destroy()
+
+    def add_widgets(self, bug):
+        #Assign Labels
+        bug_type = tk.Label(self.frame, text = "Type:", width = 10, padx = 10, pady = 10)
+        bug_type.grid(in_= self.frame, row = 0, column = 0, sticky = tk.E)
+
+        title = tk.Label(self.frame, text = "Title:", width = 10, padx = 10, pady = 10)
+        title.grid(in_ = self.frame, row = 1, column = 0, sticky = tk.E)
+
+        assigned_to = tk.Label(self.frame, text = "Assigned to:", width  = 10 , padx = 10, pady = 10)
+        assigned_to.grid(in_ = self.frame, row = 2, column = 0, sticky = tk.E)
+
+        priority = tk.Label(self.frame, text = "Priority:", width = 10, padx = 10, pady = 10)
+        priority.grid(in_ = self.frame, row = 3, column = 0, sticky = tk.E)
+
+        bug_status = tk.Label(self.frame, text = "Status:", width = 10, padx = 10, pady= 10)
+        bug_status.grid(in_ = self.frame, row = 4, column = 0, sticky = tk.E)
+
+        description = tk.Label(self.frame, text = "Description:", width = 10, padx = 10, pady = 10)
+        description.grid(in_ = self.frame, row = 5, column = 0, sticky = tk.E)
+        
+
+        #Assign components
+        self.tkvar1 = tk.StringVar()
+        choices = {"Defect", "Enchancement", "Task"}
+        if self._bug.types == "Defect":
+            self.tkvar1.set("Defect")
+        elif self._bug.types == "Enchancement":
+            self.tkvar1.set("Enchancement")
+        elif self._bug.types == "Task":
+            self._tkvar1.set("Task")
+        else:
+            self.tkvar1.set("<Select an option>")
+        self.type_option = tk.OptionMenu(self.frame, self.tkvar1, *choices)
+        self.type_option.grid(in_ = self.frame, row = 0, column = 1, sticky = tk.W)
+
+        self.title_entry = tk.Entry(self.frame, width = 30)
+        self.title_entry.grid(in_ = self.frame, row = 1, column = 1, sticky = tk.W)
+        if self._bug.title != "":
+            self.title_entry.insert(tk.END, self._bug.title)
+    
+
+        self.assigned_to_entry = tk.Entry(self.frame, width = 30)
+        self.assigned_to_entry.grid(in_ = self.frame, row = 2, column = 1, sticky = tk.W)
+        if self._bug.assigned_to != "":
+            self.assigned_to_entry.insert(tk.END, self._bug.assigned_to)
+
+        self.tkvar2 = tk.StringVar()
+        selection = {"Low", "Medium", "High"}
+        if self._bug.priority == "Low":
+            self.tkvar2.set("Low")
+        elif self._bug.priority == "Medium":
+            self.tkvar2.set("Medium")
+        elif self._bug.priority == "High":
+            self.tkvar2.set("High")
+        else:
+            self.tkvar2.set("<Select an option>")
+        self.priority_option = tk.OptionMenu(self.frame, self.tkvar2, *selection)
+        self.priority_option.grid(in_ = self.frame, row = 3, column = 1, sticky = tk.W)
+
+        self.tkvar3 = tk.StringVar()
+        options = {"New", "Assigned", "Closed", "Reopened", "Fixed", "Invalid", "Duplicate", "Won't fix"}
+        if self._bug.status == "New":
+            self.tkvar3.set("New")
+        elif self._bug.status == "Assigned":
+            self.tkvar3.set("Assigned")
+        elif self._bug.status == "Closed":
+            self.tkvar3.set("Closed")
+        elif self._bug.status == "Reopened":
+            self.tkvar3.set("Reopened")
+        elif self._bug.status == "Fixed":
+            self.tkvar3.set("Fixed")
+        elif self._bug.status == "Invalid":
+            self.tkvar3.set("Invalid")
+        elif self._bug.status == "Duplicate":
+            self.tkvar3.set("Duplicate")
+        elif self._bug.status == "Won't fix":
+            self.tkvar3.set("Won't fix")
+        else:
+            self.tkvar3.set("<Select an option>")
+        self.bug_status_option = tk.OptionMenu(self.frame, self.tkvar3, *options)
+        self.bug_status_option.grid(in_ = self.frame, row = 4, column = 1, sticky = tk.W)
+
+        self.description_text = tk.Text(self.frame, width = 30, height = 5)
+        self.description_text.grid(in_ = self.frame, row = 5, column = 1, sticky = tk.W)
+        if self._bug.description != "":
+            self.description_text.insert(tk.END, self._bug.description)
+        return -1
 
     def save_bugs(self):
-        pass
+        self._bug.types = self.tkvar1.get()
 
-    def close(self):
-        pass
+        if self.title_entry.get() != "":
+            self._bug.title = self.title_entry.get()
+        else:
+            self._bug.title = "<None>"
 
+        if self.assigned_to_entry.get() != "":
+            self._bug.assigned_to = self.assigned_to_entry.get()
+        else:
+            self._bug.assigned_to = "<None>"
+
+        self._bug.priority = self.tkvar2.get()
+        self._bug.status = self.tkvar3.get()
+
+        if self.description_text.get("1.0", "end-1c") != "":
+            self._bug.description = self.description_text.get("1.0", "end-1c")
+        else:
+            self._bug.description = "<No Description>"
+
+        self.saveAction(self._bug)
+        self.close()
 
 if __name__ == '__main__':
-    print('Connecting to database...')
-    connection = mysql.connector.connect(user='root',
-                                   database='bug_tracker',
-                                   charset='utf8')
-    print('Database connected')
-    application = Menu(connection)
-    application.start()
-    conn.close()
+    print("Intialising application")
+    connection = mysql.connector.connect(user='root', database='bug_tracker', charset='utf8')
+    print("Application connected")
+    Menu(connection)
+    connection.close()
+
+
--- report.py
+++ report.py
@@ -1,26 +1,34 @@
 class Data(object):
-    def __init__(self, types, title, id = None):
+    def __init__(self, id, types, title, assigned_to, priority, status, description):
         self.id = id
         self.types = types
         self.title = title
-        self.assigned_to = None
-        self.priority = None
-        self.status = None
-        self.description = None
+        self.assigned_to = assigned_to
+        self.priority = priority
+        self.status = status
+        self.description = description
 
 class Bug(object):
     def __init__(self, connection):
         self._connection = connection
     
-    def add(self):
-        pass
+    def add(self, bug):
+        cursor = self._connection.cursor()
+        query = "INSERT INTO bugtracker(bug_type, title, assigned_to, priority, bug_status, description) VALUES(%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (bug.types, bug.title, bug.assigned_to, bug.priority, bug.status, bug.description))
+        cursor.close()
+        self._connection.commit()
+
 
     def list_bugs(self):
         cursor = self._connection.cursor()
         query = "SELECT id, bug_type, title, assigned_to, priority, bug_status, description FROM bugtracker ORDER BY id"
         cursor.execute(query)
         for (id, types, title, assigned_to, priority, status, description) in cursor:
-            bug = Data(types, title, id)
+            bug = Data(id, types, title, assigned_to, priority, status, description)
+            bug.id = id
+            bug.types = types
+            bug.title = title
             bug.assigned_to = assigned_to
             bug.priority = priority
             bug.status = status
@@ -28,8 +36,13 @@ class Bug(object):
             yield bug
         cursor.close()
     
-    def update(self):
-        pass
+    def update(self, bug):
+        cursor = self._connection.cursor()
+        query = "UPDATE bugtracker SET bug_type = %s, title = %s, assigned_to = %s, priority = %s, bug_status = %s, description = %s WHERE id = %s"
+        cursor.execute(query, (bug.types, bug.title, bug.assigned_to, bug.priority, bug.status, bug.description, bug.id))
+        cursor.close()
+        self._connection.commit()
+        
 
     def delete(self):
         pass
